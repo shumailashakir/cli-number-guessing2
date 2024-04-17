b83031e842dda40231e3cb5aaae05c4a39b7b332
shumaila shakir <160516126+shumailashakir@users.noreply.github.com>
2024-04-18 00:20:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Please guess a number between  1-6:",
+    },
+]);
+if (answers.UserGuessedNumber === randomNumber) {
+    console.log("Congtratulation! you gussed right number.");
+}
+else {
+    console.log("You gussed wrong number");
+}
+console.log(randomNumber);
